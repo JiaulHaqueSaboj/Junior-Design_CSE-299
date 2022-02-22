76f8a840795870d074c219b39eb8b02382565371
Tawsif05 <tawsifmahmud05@gmail.com>
2022-02-23 01:47:10 +0600

    model created

--- Startup_Success_Prediction/SSP/models.py
+++ Startup_Success_Prediction/SSP/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Startup(models.Model):
+
+    name = models.CharField(max_length=255)
+    start = models.DateField()
+
+    categories = (
+        ('Software', "Software"),
+        ('Web', 'Web'),
+        ('Mobile', 'Mobile'),
+        ('Enterprise', 'Enterprise'),
+        ('Advertising', 'Advertising'),
+        ('Gaming Co.', 'Gaming Co.'),
+        ('E-ommerce', 'E-commerce'),
+        ('Bio-tech', 'Bio-tech'),
+        ('Consulting', 'Consulting'),
+        ('Others', 'Others'),
+    )
+    category = models.CharField(max_length=100, choices=categories)
+
+    relationship_num = models.IntegerField(max_length=100)
+    milestones_num = models.IntegerField(max_length=100)
+    first_milestone = models.DateField()
+    last_milestone = models.DateField()
+
+    total_fund = models.IntegerField()
+    first_funding = models.DateField()
+    last_funding = models.DateField()
+    funding_round_num = models.IntegerField(max_length=100)
+    has_angel = models.BooleanField(default=False)
+    has_vc = models.BooleanField(default=False)
+
+    has_A = models.BooleanField(default=False)
+    has_B = models.BooleanField(default=False)
+    has_C = models.BooleanField(default=False)
+    has_D = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'Startup'
+
+    def __str__(self):
+        return self.name
