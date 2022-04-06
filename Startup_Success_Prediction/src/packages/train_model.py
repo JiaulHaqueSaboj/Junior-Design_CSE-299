537c247b75cc24b884f57cae8f6596c051771e7f
Tawsif05 <tawsifmahmud05@gmail.com>
2022-04-06 09:58:55 +0600

    model api creation

---
+++ Startup_Success_Prediction/src/packages/train_model.py
@@ -0,0 +1,19 @@
+
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+import pickle
+
+
+def run_model_training(X_train, X_test, y_train, y_test):
+    xgb = SVC()
+    xgb.fit(X_train, y_train)
+    xgb.score(X_test, y_test)
+
+    print("Training Accuracy :", xgb.score(X_train, y_train))
+
+    print("Testing Accuracy :", xgb.score(X_test, y_test))
+    y_pred = xgb.predict(X_test)
+    print("\n3. Model Performance: \n")
+    print(classification_report(y_test, y_pred))
+
+    return xgb
