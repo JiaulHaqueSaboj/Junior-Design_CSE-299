e0f2ee76cbdf88cd4b36f9b0f21945f9b25bb9c3
Tawsif05 <tawsifmahmud05@gmail.com>
2022-04-13 11:25:42 +0600

    SSP views updated

--- Startup/Startup_Success_Prediction/SSP/views.py
+++ Startup/Startup_Success_Prediction/SSP/views.py
@@ -73,3 +73,50 @@ def logout_request(request):
     logout(request)
     messages.info(request, "You have successfully logged out.")
     return redirect("SSP:loginPage")
+
+
+def blogs(request):
+    posts = BlogPost.objects.all()
+    posts = BlogPost.objects.filter().order_by('-dateTime')
+    return render(request, "blog.html", {'posts': posts})
+
+
+def add_blogs(request):
+    if request.method == "POST":
+        form = BlogPostForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            blogpost = form.save(commit=False)
+            blogpost.author = request.user
+            blogpost.save()
+            obj = form.instance
+            alert = True
+            return render(request, "add_blogs.html", {'obj': obj, 'alert': alert})
+    else:
+        form = BlogPostForm()
+    return render(request, "add_blogs.html", {'form': form})
+
+
+def blogs_comments(request, slug):
+    post = BlogPost.objects.filter(slug=slug).first()
+    comments = Comment.objects.filter(blog=post)
+    if request.method == "POST":
+        user = request.user
+        content = request.POST.get('content', '')
+        blog_id = request.POST.get('blog_id', '')
+        comment = Comment(user=user, content=content, blog=post)
+        comment.save()
+    return render(request, "blog_comments.html", {'post': post, 'comments': comments})
+
+
+def Delete_Blog_Post(request, slug):
+    posts = BlogPost.objects.get(slug=slug)
+    if request.method == "POST":
+        posts.delete()
+        return redirect('/')
+    return render(request, 'delete_blog_post.html', {'posts': posts})
+
+
+class UpdatePostView(UpdateView):
+    model = BlogPost
+    template_name = 'edit_blog_post.html'
+    fields = ['title', 'slug', 'content', 'image']
