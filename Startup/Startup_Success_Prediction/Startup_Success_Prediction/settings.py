33eb7391823575a1eff28d0c4c130e037cfb55c6
Tawsif05 <tawsifmahmud05@gmail.com>
2022-04-13 11:22:45 +0600

    settings updated

--- Startup/Startup_Success_Prediction/Startup_Success_Prediction/settings.py
+++ Startup/Startup_Success_Prediction/Startup_Success_Prediction/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+
     'SSP',
     "startup_api",
 
@@ -123,6 +126,9 @@ USE_TZ = True
 
 LOGIN_REDIRECT_URL = 'homepage'
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- Startup/Startup_Success_Prediction/Startup_Success_Prediction/urls.py
+++ Startup/Startup_Success_Prediction/Startup_Success_Prediction/urls.py
@@ -13,11 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('SSP.urls')),
     path('api/', include('startup_api.urls')),
 ]
+
+# Serving the media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
