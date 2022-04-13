c64973f895b1d9b59badd25e689a74023fc4a7ee
Tawsif05 <tawsifmahmud05@gmail.com>
2022-04-13 11:27:25 +0600

    added

--- Startup/Startup_Success_Prediction/SSP/admin.py
+++ Startup/Startup_Success_Prediction/SSP/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Startup
+from .models import *
 
 
 admin.site.register(Startup)
+admin.site.register(BlogPost)
+admin.site.register(Comment)
