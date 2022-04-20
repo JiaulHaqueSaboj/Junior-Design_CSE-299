626f27ac463c502cb751751284bd47951b33260e
Tawsif05 <tawsifmahmud05@gmail.com>
2022-04-20 10:05:33 +0600

    activity initialized

---
+++ Startup Success Predictor App/app/src/main/java/com/example/startupsuccesspredictorapp/Activity/PredictActivity.java
@@ -0,0 +1,14 @@
+package com.example.startupsuccesspredictorapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PredictActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_predict);
+    }
+}
